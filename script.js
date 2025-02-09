586730ecab4bb4e06c10c4386377bdf9c9b14d33
Sufiyan Munshi <101869552+sufiyan02@users.noreply.github.com>
2025-02-09 20:16:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	for(let i=0;i<str.length;i++){
+	    if(str.indexOf(str[i]) == str.indexOf(str[i])){
+	        return alert(atr[i])
+	    }
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
